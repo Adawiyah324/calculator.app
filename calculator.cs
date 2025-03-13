5bea9b7ff2d35f5ea6a17158fa59f8be7af6bc5d
Adawiyah <pandaajah947@gmail.com>
2025-03-13 10:00:34 +0700

    modif project

--- calculator.cs
+++ calculator.cs
@@ -4,26 +4,12 @@ namespace CalculatorApp
     {
         static void Main(string[] args)
         {
-            int a = 10;
-            int b = 100;
+           
         }
         static int Penambahan(int a, int b)
         {
             return a + b;
 
         }
-        static int Pengurangan(int a, int b)
-        {
-            return a - b;
-        }
-        static int Perkalian(int a, int b)
-        {
-            return a * b;
-        }
-        static int Pembagian(int a, int b)
-        {
-            return a / b;
-        }
-
     }
 }
